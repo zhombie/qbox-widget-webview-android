156dad1f0d69856807f9ce8dd85f49b200c6a9b8
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2022-09-18 02:37:34 +0600

    local.properties

--- sample/src/main/java/kz/qbox/widget/webview/sample/MainActivity.kt
+++ sample/src/main/java/kz/qbox/widget/webview/sample/MainActivity.kt
@@ -3,6 +3,7 @@ package kz.qbox.widget.webview.sample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kz.qbox.widget.webview.core.WebViewActivity
+import kz.qbox.widget.webview.core.Widget
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,10 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Widget.isLoggingEnabled = true
+
         startActivity(
             WebViewActivity.newIntent(
-                this,
-                "https://kenes.vlx.kz/widget/external/enis?topic=enotary_warranty"
+                context = this,
+                url = BuildConfig.WIDGET_URL
             )
         )
     }
