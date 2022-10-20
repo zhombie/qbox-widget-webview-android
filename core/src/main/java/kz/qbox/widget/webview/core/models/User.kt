f462a781d5a2d9f903c8c673b861e49c6fa9b2ea
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2022-10-20 14:44:45 +0600

    User's device

--- core/src/main/java/kz/qbox/widget/webview/core/models/User.kt
+++ core/src/main/java/kz/qbox/widget/webview/core/models/User.kt
@@ -9,8 +9,55 @@ data class User constructor(
     val lastName: String? = null,
     val patronymic: String? = null,
     val iin: String? = null,
-    val phoneNumber: String? = null
+    val phoneNumber: String? = null,
+    val device: Device? = null
 ) : JSONObjectable, Serializable {
+
+    data class Device constructor(
+        private val os: String,
+        private val osVersion: String,
+        private val appVersion: String,
+        private val name: String,
+        private val mobileOperator: String,
+        private val battery: Battery
+    ) : JSONObjectable {
+
+        data class Battery constructor(
+            private val percentage: Double,
+            private val isCharging: Boolean,
+            private val temperature: Float
+        ) : JSONObjectable {
+
+            override fun toJSONObject(): JSONObject {
+                val jsonObject = JSONObject()
+                try {
+                    jsonObject.put("percentage", percentage)
+                    jsonObject.put("is_charging", isCharging)
+                    jsonObject.put("temperature", temperature)
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+                return jsonObject
+            }
+
+        }
+
+        override fun toJSONObject(): JSONObject {
+            val jsonObject = JSONObject()
+            try {
+                jsonObject.put("os", os)
+                jsonObject.put("osVersion", osVersion)
+                jsonObject.put("appVersion", appVersion)
+                jsonObject.put("name", name)
+                jsonObject.put("mobileOperator", mobileOperator)
+                jsonObject.put("battery", battery.toJSONObject())
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+            return jsonObject
+        }
+
+    }
 
     override fun toJSONObject(): JSONObject {
         val jsonObject = JSONObject()
@@ -20,6 +67,7 @@ data class User constructor(
             jsonObject.put("patronymic", patronymic)
             jsonObject.put("iin", iin)
             jsonObject.put("phone_number", phoneNumber)
+            jsonObject.put("device", device?.toJSONObject())
         } catch (e: JSONException) {
             e.printStackTrace()
         }
